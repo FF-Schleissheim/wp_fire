0813315d0ef9396674778837d0a12e233fb5d447
h0p3zZ <danielwimmer08012003@gmail.com>
2022-12-22 19:37:09 +0100

    implemented and designed basic footer

--- patterns/footer-default.php
+++ patterns/footer-default.php
@@ -8,17 +8,62 @@
 ?>
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group">
-	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40)">
-		<!-- wp:site-title {"level":0} /-->
-		<!-- wp:paragraph {"align":"right"} -->
-		<p class="has-text-align-right">
-			<?php
-				print "© 2022 -"; echo date("Y")
-			?>
-		</p>
-		<!-- /wp:paragraph -->
+	<!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between","verticalAlignment":"top"}} -->
+	<div class="wp-block-group alignwide">
+		<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+		<div class="wp-block-group">
+			<!-- wp:site-title {"level":6} /-->
+			<!-- wp:paragraph -->
+			<p><a href="">Tel: +43 7242 / 42420-122</a></p>
+			<!-- TODO: get actual phone number and put it into the href -->
+			<!-- /wp:paragraph -->
+			<!-- wp:social-links -->
+			<ul class="wp-block-social-links">
+				<!-- wp:social-link {"url":"https://www.facebook.com/","service":"facebook"} /-->
+				<!-- wp:social-link {"url":"https://www.instagram.com/","service":"instagram"} /-->
+			</ul>
+			<!-- /wp:social-links -->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+		<div class="wp-block-group">
+			<!-- wp:heading {"level":6} -->
+			<h6>Kontakt</h6>
+			<!-- /wp:heading -->
+			<!-- wp:paragraph -->
+			<p>
+				<a href="https://goo.gl/maps/pzY4cafv16or5Yv29" style="text-decoration: none;" target="_blank" rel="noreferrer noopener">
+				Freiwillige Feuerwehr Schleißheim<br>
+				Dorfstraße 14<br>
+				4600 Schleißheim<br>
+				Oberösterreich
+				</a>
+			</p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+		<div class="wp-block-group">
+			<!-- wp:heading {"level":6} -->
+			<h6>Rechtliches</h6>
+			<!-- /wp:heading -->
+			<!-- wp:paragraph -->
+			<p><a href="?page_id=">Impressum</a></p>
+			<!-- TODO: get Impressum page id and put it into the href -->
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
 	</div>
 	<!-- /wp:group -->
+	<!-- wp:separator -->
+	<hr class="wp-block-separator has-alpha-channel-opacity"/>
+	<!-- /wp:separator -->
+	<!-- wp:paragraph {"style":{"color":{"text":"#686872"}},"align":"right","fontSize":"small","align":"wide"} -->
+	<p class="has-text-align-right has-small-font-size alignwide" style="color:#686872">
+		<?php
+			print "Copyright © 2022 - "; echo date("Y");
+		?>
+	</p>
+	<!-- /wp:paragraph -->
 </div>
 <!-- /wp:group -->
