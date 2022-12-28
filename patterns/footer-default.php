5870c4db7b1de957b1de6a7a9dc5e024c1144da6
h0p3zZ <danielwimmer08012003@gmail.com>
2022-12-28 02:46:09 +0100

    added {CHANGE:}-tags and changed to correct phone number

--- patterns/footer-default.php
+++ patterns/footer-default.php
@@ -14,11 +14,12 @@
 		<div class="wp-block-group">
 			<!-- wp:site-title {"level":6} /-->
 			<!-- wp:paragraph -->
-			<p><a href="">Tel: +43 7242 / 42420-122</a></p>
-			<!-- TODO: get actual phone number and put it into the href -->
+			<!-- {CHANGE:To your phone number with correct href} -->
+			<p><a href="tel:+43724242420,122">Tel: +43 7242 / 42420-122</a></p>
 			<!-- /wp:paragraph -->
 			<!-- wp:social-links -->
 			<ul class="wp-block-social-links">
+				<!-- {CHANGE:facebokk and instagram link} -->
 				<!-- wp:social-link {"url":"https://www.facebook.com/","service":"facebook"} /-->
 				<!-- wp:social-link {"url":"https://www.instagram.com/","service":"instagram"} /-->
 			</ul>
@@ -30,8 +31,10 @@
 			<!-- wp:heading {"level":6} -->
 			<h6>Kontakt</h6>
 			<!-- /wp:heading -->
+			
 			<!-- wp:paragraph -->
 			<p>
+				<!-- {CHANGE:google maps link and address} -->
 				<a href="https://goo.gl/maps/pzY4cafv16or5Yv29" style="text-decoration: none;" target="_blank" rel="noreferrer noopener">
 				Freiwillige Feuerwehr Schleißheim<br>
 				Dorfstraße 14<br>
